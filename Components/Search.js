eaf3fc7a9649d45cf595e756bdaf117a7986ea8a
blagisquet <baptiste.lagisquet@imie.fr>
2020-02-21 11:20:15 +0100

    added call from API

--- Components/Search.js
+++ Components/Search.js
@@ -1,21 +1,36 @@
 import React from 'react';
-import  { StyleSheet, View, TextInput, Button, Text, FlatList } from 'react-native';
+import { StyleSheet, View, TextInput, Button, Text, FlatList } from 'react-native';
 
 import FilmItem from './FilmItem';
 
 import data from '../Helpers/filmsData';
 
-export default function Search() {
-  return (
-    <View style={styles.mainContainer}>
-      <TextInput placeholder="Titre du film" style={styles.textinput} />
-      <Button title="Rechercher" onPress={() => {}} />
-      <FlatList 
-      data={data} 
-      keyExtractor={(item) => item.id.toString()}
-      renderItem={({item}) => <FilmItem film={item} />} />
-    </View>
-  );
+import { getFilmsFromApiWithSearchedText } from '../API/TMDBApi';
+
+export default class Search extends React.Component {
+  constructor(props) {
+    super(props);
+    this._films = [];
+  }
+
+  _loadFilms() {
+    getFilmsFromApiWithSearchedText("star").then(data => {
+      this._films = data.results;
+      this.forceUpdate();
+    });
+  }
+  render() {
+    return (
+      <View style={styles.mainContainer}>
+        <TextInput placeholder="Titre du film" style={styles.textinput} />
+        <Button title="Rechercher" onPress={() => { this._loadFilms() }} />
+        <FlatList
+          data={this._films}
+          keyExtractor={(item) => item.id.toString()}
+          renderItem={({ item }) => <FilmItem film={item} />} />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
