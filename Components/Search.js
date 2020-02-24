49774cd330c978ad062190f1dce9e9b76e8cd850
blagisquet <baptiste.lagisquet@imie.fr>
2020-02-24 11:53:24 +0100

    added infinite scroll

--- Components/Search.js
+++ Components/Search.js
@@ -9,6 +9,8 @@ export default class Search extends React.Component {
   constructor(props) {
     super(props);
     this.searchedText = '';
+    this.page = 0;
+    this.totalPages = 0;
     this.state = {
       films: [],
       isLoading: false
@@ -16,7 +18,7 @@ export default class Search extends React.Component {
   }
 
   _displayLoading() {
-    if(this.state.isLoading) {
+    if (this.state.isLoading) {
       return (
         <View style={styles.loadingContainer}>
           <ActivityIndicator size='large' />
@@ -28,13 +30,26 @@ export default class Search extends React.Component {
   _loadFilms() {
     if (this.searchedText.length > 0) {
       this.setState({ isLoading: true })
-      getFilmsFromApiWithSearchedText(this.searchedText).then(data => {
+      getFilmsFromApiWithSearchedText(this.searchedText, this.page + 1).then(data => {
+        this.page = data.page;
+        this.totalPages = data.total_pages;
         this.setState({
-          films: data.results,
+          films: [...this.state.films, ...data.results],
           isLoading: false
         })
       })
     }
+  }
+
+  _searchFilms() {
+    this.page = 0;
+    this.totalPages = 0;
+    this.setState({
+      films: []
+    }, () => {
+      console.log("Page : " + this.page + " / TotalPages : " + this.totalPages + " / Nombre de films : " + this.state.films.length)
+      this._loadFilms()
+    })
   }
 
   _searchTextInputChanged(text) {
@@ -42,7 +57,6 @@ export default class Search extends React.Component {
   }
 
   render() {
-    console.log(this.state.isLoading);
     return (
       <View style={styles.mainContainer}>
         <TextInput
@@ -51,11 +65,17 @@ export default class Search extends React.Component {
           onChangeText={(text) => this._searchTextInputChanged(text)}
           onSubmitEditing={() => this._loadFilms()}
         />
-        <Button title="Rechercher" onPress={() => { this._loadFilms() }} />
+        <Button title="Rechercher" onPress={() => { this._searchFilms() }} />
         <FlatList
           data={this.state.films}
           keyExtractor={(item) => item.id.toString()}
-          renderItem={({ item }) => <FilmItem film={item} />} 
+          renderItem={({ item }) => <FilmItem film={item} />}
+          onEndReachedThreshold={0.5}
+          onEndReached={() => {
+            if (this.page < this.totalPages) {
+              this._loadFilms()
+            }
+          }}
         />
         {this._displayLoading()}
       </View>
